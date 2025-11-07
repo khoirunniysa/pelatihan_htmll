5d73f7291d930d835f0cd9010b2e7c0d94009b0f
khoirunniysa <khoirunniysa515@gmail.com>
2025-11-07 09:23:57 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document.getElementById('sapaButton');
-tombolSapa.addEventListener('click', function() {
-  alert('Hallo!, Thanks ya udah lihat Instagram ku!!');
+const tombolSapa = document.getElementById('sapaButton');//pilih tombol berdasarkan ID-Nya.
+tombolSapa.addEventListener('click',function(){//tambahkan 'event listener'untuk aksi 'click'.
+  alert('Halo!,Thanks ya udah mampir!!');//jalankan kode ini ketika tombol di klik.
 });
