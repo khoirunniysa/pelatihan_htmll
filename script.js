adf34a93f96c9ea5a9e01dfa3eddda754a77a63c
khoirunniysa <khoirunniysa515@gmail.com>
2025-11-07 10:31:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-// Ambil elemen tombol berdasarkan ID
 const tombolSapa = document.getElementById('sapaButton');
 
-// Tambahkan event listener untuk klik tombol
 tombolSapa.addEventListener('click', function() {
   alert('Halo! Terima kasih sudah mengunjungi website saya! 😊');
 });
